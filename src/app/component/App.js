1f36b57d2af1411d440b8c26c4f2c6b9d82c597c
kattisarika <sarika.katti@gmail.com>
2017-03-23 15:47:48 -0700

    latest changes

--- src/app/component/App.js
+++ src/app/component/App.js
@@ -12,50 +12,7 @@ class App extends React.Component{
   
 
 render(){
-   var footerstyle={
-	   	background:'#104E8B',
-	    color:'white',
-	    width: '100%',
-	    height:'40px',
-	    marginTop:'570px'
-		}
-
-    var header={   
-	    textAlign:'center',
-	    verticalAlign:'center',
-	    fontSize:'25px',
-	    background:'#104E8B',
-	    color:'white',
-	    width: '100%',
-	    height:'60px',
-	    margin: '0',
-	    padding: '0',
-	    border: '0'
-    }
-
-   var heading={    				
-	    listStyle:'none',
-	    padding:'10px',
-      textAlign:'center',
-      verticalAlign:'center'
-   }
-   
-
-    var menu={
-    	float:'right',
-    	marginRight:'100px',
-	    listStyle:'none',
-	     padding:'10px'
-
-    }
-
-    var linkstyle={
-    	 color:'white',
-	    fontSize:'18px'
-    }
-
-   
-
+  
    return (
           <div className="row">
              
